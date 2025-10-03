95c0f8f23e0f2f988b7bda5baaafa11f46cb6184
kevin <kevingerhardt09@t-online.de>
2025-10-03 14:06:57 +0200

    add wind to lunarlander

--- slimdqn/environments/lunar_lander.py
+++ slimdqn/environments/lunar_lander.py
@@ -3,8 +3,12 @@ import numpy as np
 
 
 class LunarLander:
-    def __init__(self, render_mode=None):
-        self.env = gym.make("LunarLander-v3", render_mode=render_mode)
+    def __init__(self, render_mode=None, stochastic: bool = False):
+        self.env = (
+            gym.make("LunarLander-v3", render_mode=render_mode, enable_wind=True, wind_power=10, turbulence_power=1)
+            if stochastic
+            else gym.make("LunarLander-v3", render_mode=render_mode)
+        )
         self.observation_shape = self.env.observation_space.shape
         self.n_actions = self.env.action_space.n
 
