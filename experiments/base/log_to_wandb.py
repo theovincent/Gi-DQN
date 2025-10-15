812b8467d38c8a7fd068a154fd7f830bdc729ab9
kevin <kevingerhardt09@t-online.de>
2025-10-15 12:06:07 +0200

    make logging script more general

--- experiments/base/log_to_wandb.py
+++ experiments/base/log_to_wandb.py
@@ -4,22 +4,22 @@ import os
 import numpy as np
 
 
-def collect_data(base_experiment_name, env_name, algorithms, seeds, lrs, tufs, n_epochs):
+def collect_data(get_experiment_name, env_name, algorithms, seeds, var1_options, var2_options, n_epochs):
     data = {}
     for algo in algorithms:
         data[algo] = {}
         for seed in seeds:
             data[algo][seed] = [{} for _ in range(n_epochs)]
-            for lr in lrs:
-                for tuf in tufs:
-                    returns = load_json_data(base_experiment_name, env_name, algo, seed, tuf, lr)["episode_returns"]
+            for v1 in var1_options:
+                for v2 in var2_options:
+                    returns = load_json_data(get_experiment_name, env_name, algo, seed, v1, v2)["episode_returns"]
                     for epoch in range(n_epochs):
-                        data[algo][seed][epoch][f"avg_return_{tuf}_{lr}"] = np.mean(returns[epoch])
+                        data[algo][seed][epoch][f"avg_return_{v1}_{v2}"] = np.mean(returns[epoch])
     return data
 
 
-def load_json_data(base_experiment_name, env_name, algo, seed, tuf, lr):
-    experiment_name = f"{base_experiment_name}_tuf{tuf}_lr{lr}"
+def load_json_data(get_experiment_name, env_name, algo, seed, v1, v2):
+    experiment_name = get_experiment_name(v1, v2)
     if algo == "gidqn_unfrozen":
         experiment_name = "unfrozen_" + experiment_name
         algo = "gidqn"
@@ -53,14 +53,15 @@ def write_data_to_wandb(base_experiment_name, data, algorithms, seeds, n_epochs)
 
 if __name__ == "__main__":
     base_experiment_name = "test_new_pipeline3"
+    get_experiment_name = lambda var1, var2: f"{base_experiment_name}_tuf{var1}_lr{var2}"
     env_name = "lunar_lander"
     algorithms = ["dqn", "dqnrc", "idqn", "fidqn", "gidqn", "gidqn_unfrozen"]
     seeds = range(1, 4)
-    lrs = ["1e-4", "1e-1"]
-    tufs = ["25", "1_000"]
+    var1_options = ["25", "1_000"]
+    var2_options = ["1e-4", "1e-1"]
     n_epochs = 25
 
     print("Checking experiments...")
-    data = collect_data(base_experiment_name, env_name, algorithms, seeds, lrs, tufs, n_epochs)
+    data = collect_data(get_experiment_name, env_name, algorithms, seeds, var1_options, var2_options, n_epochs)
     print("All experiments found. Writing to wandb...")
     write_data_to_wandb(base_experiment_name, data, algorithms, seeds, n_epochs)
