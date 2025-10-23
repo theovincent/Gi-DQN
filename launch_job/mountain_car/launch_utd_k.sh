777f35e2179573d6a46e7a2bc2871524148dcc5f
kevin <kevingerhardt09@t-online.de>
2025-10-23 14:35:54 +0200

    adjusted launch files for experiment

--- launch_job/mountain_car/launch_utd_k.sh
+++ launch_job/mountain_car/launch_utd_k.sh
@@ -1,5 +1,5 @@
 SHARED_ARGS="--replay_buffer_capacity 10_000 --batch_size 32 --update_horizon 1 --gamma 0.99 --horizon 1_000 \
-  --n_epochs 10 --n_training_steps_per_epoch 10_000 --n_initial_samples 1_000 --epsilon_end 0.01 \
+  --n_epochs 10 --n_training_steps_per_epoch 5_000 --n_initial_samples 1_000 --epsilon_end 0.01 \
   --epsilon_duration 1_000 --architecture_type fc"
 
 TARGET_SYNC_FREQ=5
@@ -10,9 +10,9 @@ N_BELLMAN_ITERATIONS=5
 WEIGHT_DECAY=1
 MU=1
 
-# np.logspace(np.log10(start), np.log10(stop), 10)
-UPDATE_TO_DATA=()
-FEATURES=(5 8 12 20 31 49 77 121 190 300)
+# np.logspace(np.log10(start), np.log10(stop), 5)
+UPDATE_TO_DATA=(1 3 5 8 10)
+FEATURES=(5 14 39 108 300)
 
 
 PLATFORM="stud/cluster"  # stud/cluster local/local
