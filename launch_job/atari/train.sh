58f44927c080c139a0dc2568e006150ee50f5e22
Theo <theovincentjourdat@gmail.com>
2026-01-07 10:03:50 +0100

    fix bug launcher

--- launch_job/atari/train.sh
+++ launch_job/atari/train.sh
@@ -3,7 +3,7 @@
 source launch_job/parse_arguments.sh
 parse_arguments $@ --first_seed dummy --last_seed dummy
 FIRST_SEED=$((N_PARALLEL_SEEDS * (SLURM_ARRAY_TASK_ID - 1) + 3)) 
-LAST_SEED=$((N_PARALLEL_SEEDS * SLURM_ARRAY_TASK_ID) + 2)
+LAST_SEED=$((N_PARALLEL_SEEDS * SLURM_ARRAY_TASK_ID + 2))
 
 source env/bin/activate
 export XLA_PYTHON_CLIENT_MEM_FRACTION=$(echo "scale=2 ; 1 / ($LAST_SEED - $FIRST_SEED + 1)" | bc)
