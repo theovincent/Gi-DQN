79c80f952a608723609726b2b21ad380a0980b8d
Théo Vincent <tvincent@virt-3326.kl.dfki.de>
2026-01-08 15:05:37 +0100

    pegasus cluster

---
+++ launch_job/atari/pegasus/interactive_node.sh
@@ -0,0 +1,5 @@
+srun --job-name "requirements" --cpus-per-task 4 --mem-per-cpu 8G --time 4:00:00 --immediate=3600 \
+    --container-mounts=/netscratch/$USER/Gi-DQN:/netscratch/$USER/Gi-DQN --container-image=/enroot/nvcr.io_nvidia_pytorch_23.12-py3.sqsh \
+    --container-workdir=/netscratch/$USER/Gi-DQN --gres=gpu:1 --pty bash
+
+apt-get install ffmpeg libsm6 libxext6 bc -y
