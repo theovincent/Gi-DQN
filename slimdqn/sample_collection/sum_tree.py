16b2df22a9dc035babdef491d45886f627cff832
Theo Vincent <theovincentjourdat@gmail.com>
2026-01-13 15:23:50 +0100

    fix tree bug

--- slimdqn/sample_collection/sum_tree.py
+++ slimdqn/sample_collection/sum_tree.py
@@ -47,8 +47,8 @@ class SumTree:
 
             # Traverse the tree but only if that index isn't masked
             node_indices = np.where(
-                traversal_mask, np.where(targets < left_node_sums, left_node_indices, right_node_indices), node_indices
+                traversal_mask, np.where(targets <= left_node_sums, left_node_indices, right_node_indices), node_indices
             )
-            targets = np.where(targets < left_node_sums, targets, targets - left_node_sums)
+            targets = np.where(targets <= left_node_sums, targets, targets - left_node_sums)
 
         return node_indices - self.first_leaf_offset
