a15a7e07d0569a07e78f04d346d5527645f23db0
yogesh1q2w <yogesh120699@gmail.com>
2026-03-02 15:20:42 +0100

    time DQN algorithms

---
+++ tests/flop_comparison/time_algorithms.py
@@ -0,0 +1,55 @@
+import json
+import os
+import shutil
+import subprocess
+import time
+
+import numpy as np
+
+
+def run_algorithm(algo_name, algo_args):
+    print(f"\n\n\n--------------- Time {algo_name} ---------------", flush=True)
+    save_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), f"../../experiments/atari/exp_output/_time_{algo_name}_Breakout"
+    )
+    if os.path.exists(save_path):
+        shutil.rmtree(save_path)
+
+    time_begin = time.time()
+    returncode = subprocess.run(
+        f"python3 experiments/atari/{algo_name}.py --experiment_name _time_{algo_name}_Breakout {algo_args}".split(" ")
+    ).returncode
+    time_end = time.time()
+
+    if returncode != 0:
+        print(
+            f"Training {algo_name} should not have raised an error. The training time has to be recomputed.", flush=True
+        )
+    else:
+        print(f"{algo_name} trained in {np.around(time_end - time_begin)} seconds.", flush=True)
+
+    shutil.rmtree(save_path)
+
+    return time_end - time_begin if returncode == 0 else None
+
+
+if __name__ == "__main__":
+    base_args = (
+        "--seed 1 --disable_wandb --replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon 1 --gamma 0.99 "
+        + "--horizon 27_000 --target_update_period 8000 --n_epochs 2 --n_training_steps_per_epoch 250_000 --update_to_data 0.25 "
+        + "--architecture_type cnn --features 32 64 64 512 "
+        + "--n_initial_samples 20_000 --epsilon_end 0.01 --epsilon_duration 1 --learning_rate 6.25e-5"
+    )
+
+    time_dqn = run_algorithm("dqn", base_args)
+    time_dqnrcshared = run_algorithm("dqnrcshared", base_args + " --weight_decay 1 --linear_heads")
+    time_idqnshared = run_algorithm("idqnshared", base_args + " --n_bellman_iterations 5 --linear_heads")
+    time_gidqnshared = run_algorithm(
+        "gidqnshared", base_args + " --n_bellman_iterations 5 --weight_decay 1 --linear_heads"
+    )
+
+    json.dump(
+        {"dqn": time_dqn, "dqnrcshared": time_dqnrcshared, "idqn": time_idqnshared, "gidqn": time_gidqnshared},
+        open("tests/time_computation/time_algorithms.json", "w"),
+        indent=4,
+    )
