da67b09f5b70afc78c12a139b4f7ef1fdbb460e1
yogesh1q2w <yogesh120699@gmail.com>
2026-03-04 16:16:12 +0100

    add impala time computation

--- tests/flop_comparison/time_algorithms.py
+++ tests/flop_comparison/time_algorithms.py
@@ -34,22 +34,45 @@ def run_algorithm(algo_name, algo_args):
 
 
 if __name__ == "__main__":
-    base_args = (
-        "--seed 1 --disable_wandb --replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon 1 --gamma 0.99 "
-        + "--horizon 27_000 --target_update_period 8000 --n_epochs 1 --n_training_steps_per_epoch 250_000 --update_to_data 0.25 "
-        + "--architecture_type cnn --features 32 64 64 512 "
-        + "--n_initial_samples 32 --epsilon_end 0.01 --epsilon_duration 1 --learning_rate 6.25e-5"
+    base_args_cnn = (
+        "--seed 1 --disable_wandb --replay_buffer_capacity 1_000_000 --batch_size 32 --gamma 0.99 --horizon 27_000 "
+        + "--n_epochs 1 "  # reduce to 1
+        + "--n_training_steps_per_epoch 250_000 "
+        + "--n_initial_samples 32 "  # reduce to 32
+        + "--epsilon_end 0.01 "
+        + "--epsilon_duration 1 "  # reduce to 1
+        + "--learning_rate 6.25e-5 --features 32 64 64 512 --target_update_period 8000 --architecture_type cnn --update_to_data 0.25 --update_horizon 1"
     )
 
-    time_dqn = run_algorithm("dqn", base_args)
-    time_dqnrcshared = run_algorithm("dqnrcshared", base_args + " --weight_decay 1 --linear_heads")
-    time_idqnshared = run_algorithm("idqnshared", base_args + " --n_bellman_iterations 5 --linear_heads")
-    time_gidqnshared = run_algorithm(
-        "gidqnshared", base_args + " --n_bellman_iterations 5 --weight_decay 1 --linear_heads"
+    time_cnn_q = run_algorithm("dqn", base_args_cnn)
+    time_cnn_qrc = run_algorithm("dqnrcshared", base_args_cnn + " --linear_heads --weight_decay 1")
+    time_cnn_iq = run_algorithm("idqnshared", base_args_cnn + " --linear_heads --n_bellman_iterations 5")
+    time_cnn_giq = run_algorithm(
+        "gidqnshared", base_args_cnn + " --linear_heads --n_bellman_iterations 5 --weight_decay 1 --freeze_first_head"
     )
 
-    json.dump(
-        {"dqn": time_dqn, "dqnrcshared": time_dqnrcshared, "idqn": time_idqnshared, "gidqn": time_gidqnshared},
-        open("tests/flop_comparison/time_algorithms.json", "w"),
-        indent=4,
+    results = {"cnn+dqn": {"q": time_cnn_q, "qrc": time_cnn_qrc, "iq": time_cnn_iq, "giq": time_cnn_giq}}
+
+    base_args_impala = (
+        "--seed 1 --disable_wandb --replay_buffer_capacity 1_000_000 --batch_size 32 --gamma 0.99 --horizon 27_000 "
+        + "--n_epochs 1 "  # reduce to 1
+        + "--n_training_steps_per_epoch 250_000 "
+        + "--n_initial_samples 32 "  # reduce to 32
+        + "--epsilon_end 0.01 "
+        + "--epsilon_duration 1 "  # reduce to 1
+        + "--learning_rate 6.25e-5 --features 16 32 32 512 --gap --per --target_update_period 8000 --architecture_type impala --update_to_data 0.25 --update_horizon 3"
     )
+
+    time_impala_q = run_algorithm("dqn", base_args_impala)
+    time_impala_qrc = run_algorithm("dqnrcshared", base_args_impala + " --linear_heads --weight_decay 1")
+    time_impala_iq = run_algorithm("idqnshared", base_args_impala + " --linear_heads --n_bellman_iterations 5")
+    time_impala_giq = run_algorithm(
+        "gidqnshared",
+        base_args_impala + " --linear_heads --n_bellman_iterations 5 --weight_decay 1 --freeze_first_head",
+    )
+
+    results.update(
+        {"impala+dqn": {"q": time_impala_q, "qrc": time_impala_qrc, "iq": time_impala_iq, "giq": time_impala_giq}}
+    )
+
+    json.dump(results, open("tests/flop_comparison/time_algorithms.json", "w"), indent=4)
