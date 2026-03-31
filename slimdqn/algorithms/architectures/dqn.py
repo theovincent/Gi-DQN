63d1bc717490dfe9a788ec92248cd86932c2ee5c
Habibi <h.maraqten@gmail.com>
2026-03-31 11:26:06 +0200

    fix bug

--- slimdqn/algorithms/architectures/dqn.py
+++ slimdqn/algorithms/architectures/dqn.py
@@ -163,7 +163,7 @@ class DQNNet(nn.Module):
                     name="h_heads",
                 )(jax.lax.stop_gradient(x))
             else:
-                h_vals = Head(None, self.n_h_heads * self.n_actions, initializer, False, fc=self.n_fc, name="h_heads")(
+                h_vals = Head(None, self.n_h_heads * self.n_actions, initializer, False, n_fc=self.n_fc, name="h_heads")(
                     jax.lax.stop_gradient(x)
                 ).reshape((self.n_h_heads, self.n_actions))
             return q_vals, h_vals
