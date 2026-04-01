332f9741271c017a7a89228b5393c4ce69914a77
Habibi <h.maraqten@gmail.com>
2026-04-01 16:41:36 +0200

    adapt ressources

--- launch_job/atari/cluster/cluster_idqnshared.sh
+++ launch_job/atari/cluster/cluster_idqnshared.sh
@@ -5,7 +5,7 @@ parse_arguments $@
 
 echo "launch train $ALGO_NAME"
 
-sbatch --job-name $EXPERIMENT_NAME-$ALGO_NAME --array=$FIRST_SEED-$LAST_SEED:$N_PARALLEL_SEEDS --cpus-per-task=1 --mem-per-cpu=$((N_PARALLEL_SEEDS * 1500))M --time=05:00:00 --partition stud \
+sbatch --job-name $EXPERIMENT_NAME-$ALGO_NAME --array=$FIRST_SEED-$LAST_SEED:$N_PARALLEL_SEEDS --cpus-per-task=1 --mem-per-cpu=$((N_PARALLEL_SEEDS * 1500))M --time=05:20:00 --partition stud \
 --output=experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/train_$FIRST_SEED-$LAST_SEED.out \
 launch_job/$ENV_NAME/train.sh --algo_name $ALGO_NAME --env_name $ENV_NAME --experiment_name $EXPERIMENT_NAME $ARGS --n_parallel_seeds $N_PARALLEL_SEEDS
 
