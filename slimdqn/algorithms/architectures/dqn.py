6b2d78e6dc5466a036054f6847dcc76e24f4c04c
Habib Maraqten <h.maraqten@gmail.com>
2026-04-01 16:12:40 +0200

    adapt Kernel and Stide, includes comments for found K and S settings

--- slimdqn/algorithms/architectures/dqn.py
+++ slimdqn/algorithms/architectures/dqn.py
@@ -75,8 +75,8 @@ class DQNNet(nn.Module):
             initializer = nn.initializers.xavier_uniform()
             idx_feature_start = self.n_conv
             first_kernel, first_stride = (
-                ((5, 5), (1, 1)) if self.low_scale else ((4, 4), (2, 2))  # 84 x 84 pixel: ((8, 8), (4, 4))
-            )  # for 42 x 42 pxl: ((4, 4), (2, 2))
+                ((4, 4), (2, 2)) if self.low_scale else [] # 84 x 84 pixel: ((8, 8), (4, 4))
+                )  # for 42 x 42 pxl: ((4, 4), (2, 2)); for 10 x 10 pixels: ((3, 3), (1, 1)); for 24 x 24 pixels: ((5,5), (1,1))
             x = nn.Conv(
                 features=self.features[0], kernel_size=first_kernel, strides=first_stride, kernel_init=initializer
             )(jnp.array(x, ndmin=4) / 255.0)
