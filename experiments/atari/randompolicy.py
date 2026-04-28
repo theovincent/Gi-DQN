be350e113bad6cfe6ccbe8fe975514a3e045b31a
Habib Maraqten <h.maraqten@gmail.com>
2026-04-28 16:09:10 +0200

    added random policy

---
+++ experiments/atari/randompolicy.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import jax
+import numpy as np
+
+from experiments.base.dqn import train
+from experiments.base.utils import prepare_logs
+from slimdqn.environments.atari import AtariEnv
+from slimdqn.algorithms.randompolicy import RandomPolicy
+from slimdqn.sample_collection.replay_buffer import ReplayBuffer
+from slimdqn.sample_collection.samplers import Uniform, Prioritized
+
+
+def run(argvs=sys.argv[1:]):
+    env_name, algo_name = os.path.abspath(__file__).split("/")[-2], os.path.abspath(__file__).split("/")[-1][:-3]
+    p = prepare_logs(env_name, algo_name, argvs)
+
+    q_key, train_key = jax.random.split(jax.random.PRNGKey(p["seed"]))
+
+    env = AtariEnv(
+        name=p["experiment_name"].split("_")[-1],
+        state_height_width=(p["pixels"], p["pixels"]),
+        n_stacked_frames=p["n_frame_stack"],
+        n_skipped_frames=p["n_frame_skip"],
+    )
+    rb = ReplayBuffer(
+        sampling_distribution=Prioritized(p["seed"], p["replay_buffer_capacity"]) if p["per"] else Uniform(p["seed"]),
+        batch_size=p["batch_size"],
+        max_capacity=p["replay_buffer_capacity"],
+        update_horizon=p["update_horizon"],
+        gamma=p["gamma"],
+        clipping=lambda x: np.clip(x, -1, 1),
+        stack_size=env.n_stacked_frames,
+    )
+    agent = RandomPolicy(
+        q_key,
+        (env.state_height, env.state_width, env.n_stacked_frames),
+        env.n_actions,
+    )
+    train(train_key, p, agent, env, rb)
+
+
+if __name__ == "__main__":
+    run()
+
