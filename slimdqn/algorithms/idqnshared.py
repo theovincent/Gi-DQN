5bbfb49b7abd9ac76e2b327a2bc02b0124fa08b7
Habibi <h.maraqten@gmail.com>
2026-05-05 17:34:35 +0200

    comemnt out q head logs

--- slimdqn/algorithms/idqnshared.py
+++ slimdqn/algorithms/idqnshared.py
@@ -128,10 +128,10 @@ class iDQNShared:
                 "loss": np.mean(self.cumulative_losses) / (self.target_update_period * self.update_to_data),
                 # "variance": self.cumulative_variance / (self.target_update_period * self.update_to_data),
             }
-            for idx_network in range(0, min(5, self.n_bellman_iterations)):
-                self.logs[f"networks/{idx_network}_loss"] = self.cumulative_losses[idx_network] / (
-                    self.target_update_period * self.update_to_data
-                )
+            # for idx_network in range(0, min(5, self.n_bellman_iterations)):
+            #     self.logs[f"networks/{idx_network}_loss"] = self.cumulative_losses[idx_network] / (
+            #         self.target_update_period * self.update_to_data
+            #     )
 
             self.cumulative_losses = np.zeros(self.n_bellman_iterations)
             self.cumulative_variance = 0
