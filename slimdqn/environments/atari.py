98542a47301b1a0e55035d821f58553515370ad8
Theo Vincent <theovincentjourdat@gmail.com>
2026-05-05 18:16:28 +0200

    clean architecture

--- slimdqn/environments/atari.py
+++ slimdqn/environments/atari.py
@@ -11,14 +11,7 @@ import cv2
 
 
 class AtariEnv:
-    def __init__(
-        self,
-        name: str,
-        state_height_width, #: tuple = (84, 84),
-        n_stacked_frames, #: int = 4,
-        n_skipped_frames, #: int = 4,
-        render_mode=None,
-    ) -> None:
+    def __init__(self, name: str, state_height_width, n_stacked_frames, n_skipped_frames, render_mode=None) -> None:
         self.name = name
         self.state_height, self.state_width = state_height_width[0], state_height_width[1]
         self.n_stacked_frames = n_stacked_frames
