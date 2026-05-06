8b304a3b9c9d21a546c3881696bdd4cf4fc6e34a
Theo Vincent <theovincentjourdat@gmail.com>
2026-05-06 11:47:29 +0200

    clean wandb

--- launch_job/atari/train.sh
+++ launch_job/atari/train.sh
@@ -6,7 +6,6 @@ FIRST_SEED=$SLURM_ARRAY_TASK_ID
 LAST_SEED=$((N_PARALLEL_SEEDS + SLURM_ARRAY_TASK_ID - 1))
 
 source env/bin/activate
-#export WANDB_MODE=offline
 export XLA_PYTHON_CLIENT_MEM_FRACTION=$(echo "scale=2 ; 0.98 / ($LAST_SEED - $FIRST_SEED + 1)" | bc)
 
 for (( seed=$FIRST_SEED; seed<=$LAST_SEED; seed++ ))
@@ -14,4 +13,3 @@ do
     python3 experiments/$ENV_NAME/$ALGO_NAME.py --experiment_name $EXPERIMENT_NAME --seed $seed $ARGS &> experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/train_$seed.out & 
 done
 wait
-#timemout 900 wandb sync --sync-all --include-offline || true  #only sync per-task after all seeds finished
