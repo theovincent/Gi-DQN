232a0301aa72a6ffdc326992bf5a8651fcc780c8
Theo Vincent <theovincentjourdat@gmail.com>
2026-05-06 09:52:42 +0200

    clean atari algorithms

--- experiments/atari/dqn.py
+++ experiments/atari/dqn.py
@@ -26,32 +26,24 @@ def run(argvs=sys.argv[1:]):
     )
     rb = ReplayBuffer(
         sampling_distribution=Prioritized(p["seed"], p["replay_buffer_capacity"]) if p["per"] else Uniform(p["seed"]),
-        batch_size=p["batch_size"],
         max_capacity=p["replay_buffer_capacity"],
+        batch_size=p["batch_size"],
+        stack_size=env.n_stacked_frames,
         update_horizon=p["update_horizon"],
         gamma=p["gamma"],
         clipping=lambda x: np.clip(x, -1, 1),
-        stack_size=env.n_stacked_frames,
     )
     agent = DQN(
         q_key,
         (env.state_height, env.state_width, env.n_stacked_frames),
         env.n_actions,
         features=p["features"],
-        architecture_type=p["architecture_type"],
-        layer_norm=p["layer_norm"],
-        gap=p["gap"],
         learning_rate=p["learning_rate"],
         gamma=p["gamma"],
         update_horizon=p["update_horizon"],
         update_to_data=p["update_to_data"],
         target_update_period=p["target_update_period"],
         adam_eps=1.5e-4,
-        pixels=p["pixels"],
-        kernel =p["kernel"],
-        stride=p["stride"],
-        n_conv=p["n_conv"],
-        n_fc=p["n_fc"],
     )
     train(train_key, p, agent, env, rb)
 
--- experiments/atari/dqnrcshared.py
+++ experiments/atari/dqnrcshared.py
@@ -26,22 +26,18 @@ def run(argvs=sys.argv[1:]):
     )
     rb = ReplayBuffer(
         sampling_distribution=Prioritized(p["seed"], p["replay_buffer_capacity"]) if p["per"] else Uniform(p["seed"]),
-        batch_size=p["batch_size"],
         max_capacity=p["replay_buffer_capacity"],
+        batch_size=p["batch_size"],
+        stack_size=env.n_stacked_frames,
         update_horizon=p["update_horizon"],
         gamma=p["gamma"],
         clipping=lambda x: np.clip(x, -1, 1),
-        stack_size=env.n_stacked_frames,
     )
     agent = DQNRCShared(
         q_key,
         (env.state_height, env.state_width, env.n_stacked_frames),
         env.n_actions,
         features=p["features"],
-        architecture_type=p["architecture_type"],
-        layer_norm=p["layer_norm"],
-        gap=p["gap"],
-        linear_heads=p["linear_heads"],
         learning_rate=p["learning_rate"],
         gamma=p["gamma"],
         update_horizon=p["update_horizon"],
@@ -49,11 +45,6 @@ def run(argvs=sys.argv[1:]):
         target_update_period=p["target_update_period"],
         weight_decay=p["weight_decay"],
         adam_eps=1.5e-4,
-        pixels=p["pixels"],
-        kernel=p["kernel"],
-        stride=p["stride"],
-        n_conv=p["n_conv"],
-        n_fc=p["n_fc"],
     )
     train(train_key, p, agent, env, rb)
 
