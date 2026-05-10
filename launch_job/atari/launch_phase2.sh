7b59f4b6dbef906f855778f36f9d2105ba0c0b47
Habibi <h.maraqten@gmail.com>
2026-05-10 20:28:41 +0200

    adjust phase 2 launcher

--- launch_job/atari/launch_phase2.sh
+++ launch_job/atari/launch_phase2.sh
@@ -15,21 +15,24 @@ do
     do
         for TARGET_UPDATE_PERIOD in 100 300 3000 6000 10000
         do
-            SHARED_ARGS="$SHARED_ARGS --learning_rate $LEARNING_RATE --target_update_period $TARGET_UPDATE_PERIOD"
-            SHARED_NAME="LR${LEARNING_RATE}_T${TARGET_UPDATE_PERIOD}"
+            SHARED_ARGS="$SHARED_ARGS --learning_rate $LEARNING_RATE"
+            SHARED_NAME="LR${LEARNING_RATE}"
 
             launch_job/atari/${PLATFORM}_dqn.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
-                --experiment_name ${SHARED_NAME}_${GAME}
+                --target_update_period $TARGET_UPDATE_PERIOD \
+                --experiment_name ${SHARED_NAME}_T${TARGET_UPDATE_PERIOD}_${GAME}
 
             launch_job/atari/${PLATFORM}_dqnrcshared.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
                 --weight_decay $WEIGHT_DECAY --experiment_name ${SHARED_NAME}_WD${WEIGHT_DECAY}_${GAME}
 
             launch_job/atari/${PLATFORM}_idqnshared.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
-                --n_bellman_iterations $N_BELLMAN_ITERATIONS --experiment_name ${SHARED_NAME}_K${N_BELLMAN_ITERATIONS}_${GAME}
+                --target_update_period $TARGET_UPDATE_PERIOD \
+                --n_bellman_iterations $N_BELLMAN_ITERATIONS --experiment_name ${SHARED_NAME}_T${TARGET_UPDATE_PERIOD}_K${N_BELLMAN_ITERATIONS}_${GAME}
 
             launch_job/atari/${PLATFORM}_gidqnshared.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
+                --target_update_period $TARGET_UPDATE_PERIOD\
                 --n_bellman_iterations $N_BELLMAN_ITERATIONS --weight_decay $WEIGHT_DECAY \
-                --experiment_name ${SHARED_NAME}_K${N_BELLMAN_ITERATIONS}_WD${WEIGHT_DECAY}_${GAME}
+                --experiment_name ${SHARED_NAME}_T${TARGET_UPDATE_PERIOD}_K${N_BELLMAN_ITERATIONS}_WD${WEIGHT_DECAY}_${GAME}
         done
     done
 done
