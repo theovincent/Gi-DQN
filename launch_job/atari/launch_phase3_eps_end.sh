c0f23209fbdecd4c1920dfb77e2e6ed0806427f9
Habibi <h.maraqten@gmail.com>
2026-05-16 14:08:19 +0200

    typo

--- launch_job/atari/launch_phase3_eps_end.sh
+++ launch_job/atari/launch_phase3_eps_end.sh
@@ -21,11 +21,11 @@ do
     do
     launch_job/atari/${PLATFORM}_dqnrcshared.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
         --learning_rate $LEARNING_RATE_QRC  --epsilon_end $EPS_END \
-        --weight_decay $WEIGHT_DECAY --experiment_name "LR${LEARNING_RATE_QRC}_WD${WEIGHT_DECAY}_EPSTD_END${EPS_END}_${GAME}"
+        --weight_decay $WEIGHT_DECAY --experiment_name "LR${LEARNING_RATE_QRC}_WD${WEIGHT_DECAY}_END${EPS_END}_${GAME}"
 
     launch_job/atari/${PLATFORM}_gidqnshared.sh --first_seed 1 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS \
         --learning_rate $LEARNING_RATE_GIDQN --target_update_period $TARGET_UPDATE_PERIOD_GIDQN  \
         --n_bellman_iterations $N_BELLMAN_ITERATIONS --weight_decay $WEIGHT_DECAY --epsilon_end $EPS_END \
-        --experiment_name "LR${LEARNING_RATE_GIDQN}_T${TARGET_UPDATE_PERIOD_GIDQN}_K${N_BELLMAN_ITERATIONS}_WD${WEIGHT_DECAY}_EPSTD_END${EPS_END}_${GAME}"
+        --experiment_name "LR${LEARNING_RATE_GIDQN}_T${TARGET_UPDATE_PERIOD_GIDQN}_K${N_BELLMAN_ITERATIONS}_WD${WEIGHT_DECAY}_END${EPS_END}_${GAME}"
     done
 done
