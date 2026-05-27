59bbe35f7dcd44ea24a0adeb81cfa1e7861b1db4
Theo Vincent <theovincentjourdat@gmail.com>
2026-05-27 18:08:47 +0200

    utils clean

--- slimdqn/sample_collection/utils.py
+++ slimdqn/sample_collection/utils.py
@@ -5,36 +5,34 @@ from functools import partial
 from slimdqn.sample_collection.replay_buffer import ReplayBuffer, TransitionElement
 
 
-@partial(jax.jit, static_argnames=("best_action_fn", "n_actions", "epsilon_fn"))
-def select_action(best_action_fn, params, state, key, n_actions, epsilon_fn, n_training_steps):
+@partial(jax.jit, static_argnames=("best_action_fn", "n_actions", "epsilon_fn", "epsilon_td"))
+def select_action(best_action_fn, params, state, key, n_actions, epsilon_fn, n_training_steps, epsilon_td):
     uniform_key, action_key = jax.random.split(key)
+
+    if epsilon_td:
+        best_action, explore_action = best_action_fn(params, state)
+    else:
+        best_action = best_action_fn(params, state)
+        explore_action = jax.random.randint(action_key, (), 0, n_actions)
+
     return jnp.where(
         jax.random.uniform(uniform_key) <= epsilon_fn(n_training_steps),  # if uniform < epsilon,
-        jax.random.randint(action_key, (), 0, n_actions),  # take random action
-        best_action_fn(params, state),  # otherwise, take a greedy action
-    )
-
-
-@partial(jax.jit, static_argnames=("best_action_fn", "n_actions", "epsilon_fn"))
-def select_action_epsilon_td(best_action_fn, params, state, key, n_actions, epsilon_fn, n_training_steps):
-    uniform_key, action_key = jax.random.split(key)
-    return jnp.where(
-        jax.random.uniform(uniform_key) <= epsilon_fn(n_training_steps),  # if uniform < epsilon,
-        best_action_fn(params, state)[1],  # take greedy action w.r.t H-function(s)
-        best_action_fn(params, state)[0],  # otherwise, take a greedy action
+        explore_action,  # take an exploratory action
+        best_action,  # otherwise, take a greedy action
     )
 
 
 def collect_single_sample(key, env, agent, rb: ReplayBuffer, p, epsilon_schedule, n_training_steps: int):
-
-    if p["epsilon_td"]:
-        action = select_action_epsilon_td(
-            agent.best_action, agent.params, env.state, key, env.n_actions, epsilon_schedule, n_training_steps
-        ).item()
-    else:
-        action = select_action(
-            agent.best_action, agent.params, env.state, key, env.n_actions, epsilon_schedule, n_training_steps
-        ).item()
+    action = select_action(
+        agent.best_action,
+        agent.params,
+        env.state,
+        key,
+        env.n_actions,
+        epsilon_schedule,
+        n_training_steps,
+        p.get("epsilon_td", False),
+    ).item()
 
     obs = env.observation
     reward, absorbing = env.step(action)
